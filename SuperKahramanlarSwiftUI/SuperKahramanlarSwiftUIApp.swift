e52b2970c5c1e41bf12f9750aaee237baaf67cc5
Berat Yavuz <beratttyavuzz@gmail.com>
2022-04-03 12:51:15 +0300

    Initial Commit

---
+++ SuperKahramanlarSwiftUI/SuperKahramanlarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperKahramanlarSwiftUIApp.swift
+//  SuperKahramanlarSwiftUI
+//
+//  Created by Berat Yavuz on 3.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperKahramanlarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
